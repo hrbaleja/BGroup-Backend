7e8da9b478e0245df3bb9f3e092369c2693c1acb
Hitesh Baleja <hiteshbaleja@gmail.com>
2024-07-09 00:17:43 +0530

    fix the issue

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-require('module-alias/register')
+require('module-alias/register');
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const errorHandler = require('./middleware/error');
 const apiRoutes = require('./routes/index');
 const errorMiddleware = require('./utils/errorMiddleware');
+const cors = require('cors');
+const path = require('path');
 
 // Load environment variables
 dotenv.config();
@@ -13,26 +15,26 @@ dotenv.config();
 connectDB();
 
 const app = express();
-const path = require('path');
 global.__basedir = path.resolve(__dirname);
 
 // Middleware
-const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
 // Root route
 app.get('/', (req, res) => {
     res.send('Hello B group Pvt. Ltd');
-  });
+});
+
 // Routes
 app.use('/api/v1', apiRoutes);
 
 // Error handler middleware
-app.use(errorMiddleware);
-
-app.use(errorHandler);
-
+app.use((err, req, res, next) => {
+    console.error('Error stack:', err.stack);
+    console.error('Error message:', err.message);
+    res.status(500).json({ message: err.message });
+});
 
 const PORT = process.env.PORT || 5005;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
